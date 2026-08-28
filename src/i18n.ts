cd72dc4fcaf1381446464f5098dfc7e0fbc64d91
akasshmanikandan <akasshmanikandan@users.noreply.github.com>
2026-08-28 08:37:43 +0530

    Fix TempleSilhouette SSR translation prop

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,6 +4,7 @@ import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import ta from "./locales/ta.json";
 import ml from "./locales/ml.json";
+import hi from "./locales/hi.json";
 
 i18n
   .use(initReactI18next)
@@ -12,7 +13,9 @@ i18n
       en: { translation: en },
       ta: { translation: ta },
       ml: { translation: ml },
+      hi: { translation: hi },
     },
+    supportedLngs: ["en", "ta", "ml", "hi"],
     fallbackLng: "en",
     interpolation: {
       escapeValue: false, // react already safes from xss
